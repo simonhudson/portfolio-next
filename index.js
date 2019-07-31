a3e24bd605555b4229ecf2427768101e3d716d4d
Simon Hudson <simon.hudson@experian.com>
2019-07-31 14:44:23 +0100

    del files

--- index.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-const express = require('express')
-const path = require('path')
-const PORT = process.env.PORT || 5000
-const app = express();
-
-// app.use(express.static(path.join(__dirname, '.next')));
-app.listen(PORT, () => console.log(`Listening on ${ PORT }`));
