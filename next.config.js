ed904ba78b91878f613e93bbccb43be54418420b
Simon Hudson <simon.hudson@experian.com>
2019-07-31 09:53:06 +0100

    svg loader

--- next.config.js
+++ next.config.js
@@ -5,16 +5,3 @@ const withImages = require('next-images');
 const withSass = require('@zeit/next-sass');
 
 module.exports = compose([withImages, withSass]);
-
-// module.exports = compose([
-// 	[withSass],
-// 	{
-// 		webpack(config) {
-// 			config.module.rules.push({
-// 				test: /\.svg$/,
-// 				use: ['@svgr/webpack'],
-// 			});
-// 			return config;
-// 		}
-// 	}
-// ]);
--- pages/index.js
+++ pages/index.js
@@ -9,7 +9,7 @@ export default () => (
 	
 	<html>
 		<Head>
-			<title></title>
+			<title>Web Design &amp Development by Simon Hudson</title>
 			<meta name="viewport" content="initial-scale=1.0, width=device-width" />
 			<meta http-equiv="X-UA-Compatible" content="ie=edge" />
 			<link href="https://fonts.googleapis.com/css?family=Open+Sans:300,400,600|Lora:400,700&display=swap" rel="stylesheet" />
