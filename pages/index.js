b6781579ba6ab18d92852945e46a5a54341dd85e
Simon Hudson <simon.hudson@experian.com>
2019-07-31 15:12:55 +0100

    header strap weight

--- pages/index.js
+++ pages/index.js
@@ -16,7 +16,7 @@ export default () => (
 			<title>Web Design &amp Development by Simon Hudson</title>
 			<meta name="viewport" content="initial-scale=1.0, width=device-width" />
 			<meta http-equiv="X-UA-Compatible" content="ie=edge" />
-			<link href="https://fonts.googleapis.com/css?family=Open+Sans:300,400,600|Lora:400,700&display=swap" rel="stylesheet" />
+			<link href="https://fonts.googleapis.com/css?family=Open+Sans:300,400,600,700,800|Lora:400,700&display=swap" rel="stylesheet" />
 			<link href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet" />
 		</Head>
 		<body>
