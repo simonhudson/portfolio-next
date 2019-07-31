bc0d49f02be05be0a7267d6a36058d48145d33df
Simon Hudson <simon.hudson@experian.com>
2019-07-31 14:28:06 +0100

    express

---
+++ index.js
@@ -0,0 +1,9 @@
+'use strict';
+
+const express = require('express')
+const path = require('path')
+const PORT = process.env.PORT || 5000
+const app = express();
+
+app.use(express.static(path.join(__dirname, 'build')));
+app.listen(PORT, () => console.log(`Listening on ${ PORT }`));
