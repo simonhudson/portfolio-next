bc9f8d4d8ce36a82658c380639d8f5aa2e7100b8
Simon Hudson <simon.hudson@experian.com>
2019-07-31 14:29:48 +0100

    update script

--- index.js
+++ index.js
@@ -5,5 +5,5 @@ const path = require('path')
 const PORT = process.env.PORT || 5000
 const app = express();
 
-app.use(express.static(path.join(__dirname, 'build')));
+app.use(express.static(path.join(__dirname, '.next')));
 app.listen(PORT, () => console.log(`Listening on ${ PORT }`));
