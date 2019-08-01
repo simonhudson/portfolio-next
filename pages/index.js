c6f5dd16187a4b1614103d626836aef56577e33f
Simon Hudson <simon.hudson@experian.com>
2019-08-01 09:03:53 +0100

    fix page title

--- pages/index.js
+++ pages/index.js
@@ -13,7 +13,7 @@ export default () => (
 	
 	<html>
 		<Head>
-			<title>Web Design &amp Development by Simon Hudson</title>
+			<title>Web Design &amp; Development by Simon Hudson</title>
 			<meta name="viewport" content="initial-scale=1.0, width=device-width" />
 			<meta http-equiv="X-UA-Compatible" content="ie=edge" />
 			<link href="https://fonts.googleapis.com/css?family=Open+Sans:300,400,600,700,800|Lora:400,700&display=swap" rel="stylesheet" />
