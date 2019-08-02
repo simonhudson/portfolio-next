b69700b058f07f05e5d31858d14209093f2d17f3
Simon Hudson <simon.hudson@experian.com>
2019-08-02 11:44:56 +0100

    jest wip

---
+++ jest.config.js
@@ -0,0 +1,16 @@
+'use strict';
+
+module.exports = {
+	"verbose": true,
+	"transform": {
+		"^.+\\.js$": "babel-jest"
+	},
+	"moduleFileExtensions": [
+		"js",
+		"jsx"
+	],
+	"moduleDirectories": [
+		"components",
+		"node_modules"
+	]
+};
