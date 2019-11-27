f126aee26e5bfee748c909f44d6e8ff23cbee3cb
Simon Hudson <simon.hudson@experian.com>
2019-11-27 14:57:05 +0000

    update slide

--- components/portfolio/index.js
+++ components/portfolio/index.js
@@ -33,7 +33,7 @@ class Portfolio extends Component {
         this.state.portfolioItems.forEach((item, index) => {
             const itemHeight = item.getBoundingClientRect().height;
             const itemOffsetTop = item.offsetTop;
-            const slideInAt = (window.scrollY + window.innerHeight) - itemHeight / 8;
+            const slideInAt = (window.scrollY + window.innerHeight) - itemHeight / 2;
             const itemBottom = itemOffsetTop + itemHeight;
             const isHalfShown = slideInAt > itemOffsetTop;
             const isNotScrolledPast = window.scrollY < itemBottom;
