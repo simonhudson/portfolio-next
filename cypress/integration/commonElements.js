3385d5d1404baa3c4e1d0885764a8736451996c9
Hudson <Simon.Hudson@experian.com>
2020-03-26 08:52:21 +0000

    vscode

--- cypress/integration/commonElements.js
+++ cypress/integration/commonElements.js
@@ -5,15 +5,24 @@ import { openPage } from '../custom/commands';
 beforeEach(() => openPage('home'));
 
 describe('Common page elements', () => {
-
-    it('Contains header', () => cy.contains(`Web Design & Development by Simon Hudson`));
-    it('Contains banner title', () => cy.contains(`Hi there, my name's Simon.`));
-    it('Contains banner text', () => cy.contains(`I am an experienced front-end developer who specialises in writing lean, standards-compliant HTML, CSS and JavaScript to create accessible and usable websites and applications.`));
+    it('Contains header', () =>
+        cy.contains(`Web Design & Development by Simon Hudson`));
+    it('Contains banner title', () =>
+        cy.contains(`Hi there, my name's Simon.`));
+    it('Contains banner text', () =>
+        cy.contains(
+            `I am an experienced front-end developer who specialises in writing lean, standards-compliant HTML, CSS and JavaScript to create accessible and usable websites and applications.`
+        ));
     it('Contains banner links', () => {
         [
             './Simon_Hudson_CV.pdf',
             'https://github.com/simonhudson',
             'https://stackoverflow.com/story/simonhudson'
-        ].forEach((link, index) => cy.get('.external-links__link').eq(index).should('have.attr', 'href', link));
+        ].forEach((link, index) =>
+            cy
+                .get('.external-links__link')
+                .eq(index)
+                .should('have.attr', 'href', link)
+        );
     });
 });
