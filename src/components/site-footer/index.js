612e41a1dfa06f6dd4e07c096a27669b521b9890
Hudson <Simon.Hudson@experian.com>
2020-11-18 11:29:06 +0000

    footer

--- src/components/site-footer/index.js
+++ src/components/site-footer/index.js
@@ -1,13 +1,26 @@
 import React from 'react';
 import SiteLogo from '~/components/site-logo';
-import { FooterWrap, InnerWrap } from './index.styles';
+import { FooterWrap, InnerWrap, Img, Content } from './index.styles';
 import { layout } from '~/theme';
 
 const SiteFooter = () => {
     return (
         <FooterWrap id="site-footer" data-test="site-footer">
             <layout.Wrap>
-                <InnerWrap></InnerWrap>
+                <InnerWrap>
+                    <Img alt="Simon Hudson" src="/imgs/self.jpg" />
+                    <Content>
+                        <p>
+                            Lorem ipsum dolor sit amet, consectetur adipiscing
+                            elit. Aliquam sed vestibulum dolor, vehicula
+                            condimentum massa. Vivamus pretium vel tellus sit
+                            amet imperdiet. Maecenas eu dui sed libero tempor
+                            luctus. Curabitur lobortis nisi id nunc dapibus
+                            semper. Donec in fermentum tortor, nec eleifend
+                            libero.
+                        </p>
+                    </Content>
+                </InnerWrap>
                 <SiteLogo context="footer" />
             </layout.Wrap>
         </FooterWrap>
--- src/components/site-footer/index.styles.js
+++ src/components/site-footer/index.styles.js
@@ -1,10 +1,42 @@
 import styled from 'styled-components';
+import { rem } from 'polished';
 
 const FooterWrap = styled.footer`
     background: ${({ theme }) => theme.palette.primary.white};
-    padding: ${({ theme }) => theme.padding.lg} 0;
+    padding: ${({ theme }) => theme.padding.md} 0;
 `;
 
-const InnerWrap = styled.div``;
+const InnerWrap = styled.div`
+    align-items: center;
+    display: flex;
+    flex-direction: column;
+    justify-content: space-between;
 
-export { FooterWrap, InnerWrap };
+    ${({ theme }) =>
+        theme.media(
+            'tablet-p',
+            `
+            flex-direction: row;
+            `
+        )};
+`;
+
+const Img = styled.img`
+    clip-path: polygon(50% 0%, 100% 50%, 50% 100%, 0% 50%);
+    display: block;
+    height: auto;
+    margin: 0 0 ${({ theme }) => theme.margin.md} 0;
+    width: ${rem(150)};
+
+    ${({ theme }) =>
+        theme.media(
+            'tablet-p',
+            `
+            margin: 0 ${theme.margin.md} 0 0;
+            width: ${rem(250)};
+            `
+        )};
+`;
+
+const Content = styled.div``;
+export { FooterWrap, InnerWrap, Img, Content };
