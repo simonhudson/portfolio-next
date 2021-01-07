9508fede227122258dfcf45df2616ecd1a5ead19
Simon Hudson <hellosimonhudson [at] gmail.com>
2021-01-07 09:50:27 +0000

    fix test

--- src/theme/palette.test.js
+++ src/theme/palette.test.js
@@ -4,7 +4,7 @@ describe('theme/palette', () => {
     it('should return expected values', () => {
         expect(palette).toEqual({
             primary: {
-                a: '#D65F59',
+                brand: '#D65F59',
                 bodyText: '#202020',
                 black: '#333',
                 white: '#fff',
