3ba4533811b97c4c1c969371dfdc432422f9e625
Simon Hudson <hellosimonhudson [at] gmail.com>
2021-02-03 19:54:00 +0000

    scroll test

--- src/utilities/scrollToElement.test.js
+++ src/utilities/scrollToElement.test.js
@@ -5,12 +5,22 @@ const EVENT = {
     returnValue: null,
 };
 
+const DOCUMENT = {
+    getElementById: jest.fn(),
+};
+
+const TARGET_ELEMENT = {
+    scrollIntoView: jest.fn(),
+};
+
 describe('scrollToElement', () => {
-    let e, targetId;
+    let e, targetId, document, targetElement;
 
     beforeEach(() => {
         e = EVENT;
         targetId = 'foo';
+        document = DOCUMENT;
+        targetElement = TARGET_ELEMENT;
         jest.spyOn(document, 'getElementById');
         document.getElementById.mockReturnValue(null);
     });
@@ -23,6 +33,7 @@ describe('scrollToElement', () => {
         scrollToElement(e, targetId);
         expect(e.preventDefault).toHaveBeenCalledTimes(1);
         expect(e.returnValue).toEqual(null);
+        expect(targetElement.scrollIntoView).toHaveBeenCalledTimes(0);
     });
 
     it('should handle event where preventDefault() method does not exist', () => {
@@ -32,7 +43,11 @@ describe('scrollToElement', () => {
         expect(e.returnValue).toEqual(false);
     });
 
-    it('should handle scenario where target element does not exist', () => {});
+    // it('should handle scenario where target element does not exist', () => {
+    //     expect(targetElement.scrollIntoView).toHaveBeenCalledTimes(0);
+    // });
 
-    it('should call scrollIntoView() method on target element', () => {});
+    // it('should call scrollIntoView() method on target element', () => {
+    //     document.getElementById.mockReturnValue(true);
+    // });
 });
