2f7af765bc0e5a4a08b349fa9d850e3ef70683c3
Simon Hudson <hellosimonhudson [at] gmail.com>
2021-05-07 13:36:40 +0100

    ui work

--- src/components/holding/index.js
+++ src/components/holding/index.js
@@ -1,6 +1,8 @@
 import React from 'react';
-import { Wrapper, Tagline } from './index.styles';
+import { Wrapper, Tagline, LinksList, LinksItem, LinksLink } from './index.styles';
 import SiteLogo from '~/components/site-logo';
+import data from '~/components/links/data';
+import VisuallyHidden from '~/components/utilities/visually-hidden';
 
 const Holding = () => {
 	return (
@@ -10,6 +12,18 @@ const Holding = () => {
 				<span>Web Design &amp; Development by&nbsp;</span>
 				<span>Simon Hudson</span>
 			</Tagline>
+			<LinksList>
+				{data.map((item, index) => (
+					<LinksItem key={index}>
+						<LinksLink href={item.url} target="_blank" rel="noopener noreferrer">
+							<VisuallyHidden>Visit my&nbsp;</VisuallyHidden>
+							{item.linkText}
+							<VisuallyHidden>&nbsp;profile</VisuallyHidden>
+							<VisuallyHidden>&nbsp;(opens in a new tab/window)</VisuallyHidden>
+						</LinksLink>
+					</LinksItem>
+				))}
+			</LinksList>
 		</Wrapper>
 	);
 };
--- src/components/holding/index.styles.js
+++ src/components/holding/index.styles.js
@@ -18,17 +18,36 @@ const Tagline = styled.h1`
 	}
 
 	span:first-of-type {
-		color: #2a2a2a;
+		color: #7a7a7a;
 		font-size: ${rem(16)};
 		text-transform: lowercase;
 	}
 
 	span:last-of-type {
 		color: ${({ theme }) => theme.palette.primary.bodyText};
-		font-size: ${rem(26)};
+		font-size: ${rem(36)};
 		font-weight: 500;
 		text-transform: uppercase;
 	}
 `;
 
-export { Wrapper, Tagline };
+const LinksList = styled.ul`
+	display: flex;
+	justify-content: space-around;
+	list-style: none;
+`;
+
+const LinksItem = styled.li`
+	&:not(:last-of-type) {
+		margin: 0 ${({ theme }) => theme.margin.md} 0 0;
+	}
+`;
+
+const LinksLink = styled.a`
+	color: ${({ theme }) => theme.palette.primary.brand};
+	font-weight: 500;
+	text-decoration: none;
+	text-transform: uppercase;
+`;
+
+export { Wrapper, Tagline, LinksList, LinksItem, LinksLink };
